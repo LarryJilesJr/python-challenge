c144badab7fbc604f081934a141cb31f9cbc2e89
LarryJilesJr <larryjilesjr@gmail.com>
2023-12-10 09:21:14 -0600

    Committing changes to PyBank, PyPoll, and READMe.

---
+++ PyPoll/main.py/election_poll_analysis.py
@@ -0,0 +1,54 @@
+import os
+import csv
+
+csv_file_path = "\\Users\\larry\\OneDrive\\Desktop\\vbu_mod_3\\python-challenge\\PyPoll\\main.py\\Resources\\election_data.csv"  
+output_file_path = "\\Users\\larry\\OneDrive\\Desktop\\vbu_mod_3\\python-challenge\\PyPoll\\main.py\\analysis\\election_poll_analysis_results.txt"
+
+def analyze_and_export_election_votes_data(csv_file_path, output_file_path):
+    total_votes = 0
+    candidates = {}
+    
+    with open(csv_file_path, newline="") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        header = next(csvreader) 
+
+        for row in csvreader:
+            total_votes = total_votes + 1
+
+            candidate_name = row[2]
+            if candidate_name not in candidates:
+                candidates[candidate_name] = 1
+            else:
+                candidates[candidate_name] = candidates[candidate_name] + 1
+
+    percentages = {}
+    for candidate, votes in candidates.items():
+        percentage = (votes / total_votes) * 100
+        percentages[candidate] = percentage
+
+    winner = max(candidates, key=candidates.get)
+
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+    for candidate, votes in candidates.items():
+        print(f"{candidate}: {percentages[candidate]:.3f}% ({votes})")
+    print("-------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------")
+
+    with open(output_file_path, mode="w") as textfile:
+        textfile.write("Election Results\n")
+        textfile.write("-------------------------\n")
+        textfile.write(f"Total Votes: {total_votes}\n")
+        textfile.write("-------------------------\n")
+        for candidate, votes in candidates.items():
+            textfile.write(f"{candidate}: {percentages[candidate]:.3f}% ({votes})\n")
+        textfile.write("-------------------------\n")
+        textfile.write(f"Winner: {winner}\n")
+        textfile.write("-------------------------\n")
+        
+    print(f"Election poll analysis results exported to {output_file_path}")
+
+analyze_and_export_election_votes_data(csv_file_path, output_file_path)
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # python_challenge
+PyBank Analysis Python Script:
+
+This Python script is designed to analyze financial records contained in a CSV 
+dataset. The script calculates a few key financial instances to provide insights 
+into the financial performance over a given period. The script also outputs a
+text file, which can be easily read and shared with other interested parties.
+
+The script uses Python's basic csv module and is designed to run in a Python 
+environment, but can also be adapted and integrated into other professional's 
+systems for analyzing financial data effectively.
+
+PyPoll Analysis Python Script:
+
+This Python script is designed to analyze an election dataset contained in a CSV 
+file. Using and calculating several key metrics, such as "Voter ID", 
+"Country", and "Candidate", the script provides insights into the election 
+results.
+
+This script also uses Python's standard csv module and is designed to run in a 
+Python environment. The script can be tailored and incorporated into election
+analysis systems in order to evaluate election data efficiently.
